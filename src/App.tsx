4d02b871326a4f1d2b12b048796f674821c3266f
Alejandra Hernandez <alerika@hotmail.com>
2026-09-15 19:45:15 -0700

    finish algorithms

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState, type ReactNode } from "react";
 import "./style.css";
 import ParamsVisualization from "./ParamsVisualization";
 
@@ -6,17 +6,25 @@ function App() {
   const [numbersError, setNumbersError] = useState("");
   const [targetError, setTargetError] = useState("");
   const [algorithmError, setAlgorithmError] = useState("");
+  const [resultMsg, setResultMsg] = useState<ReactNode>("");
 
   const [selectedAlgorithm, setSelectedAlgorithm] = useState<string>("0");
   const [numberGroup, setNumberGroup] = useState<string>("");
   const [target, setTarget] = useState<string>("");
 
-  const [arrNumbers, setArrNumbers] = useState<number[]>([]);
-  const [toTarget, setToTarget] = useState<number>(0);
   const [showParamsVisualization, setShowParamsVisualization] =
     useState<boolean>(false);
 
+  const [twoSumPair, setTwoSumPair] = useState<number[]>([]);
+
+  const [twoLoopsTimeConsuming, setTwoLoopsTimeConsuming] =
+    useState<ReactNode>("");
+  const [hashTimeConsuming, setHashTimeConsuming] = useState<ReactNode>("");
+
   const handleSelectedAlgorithm = useCallback((value: string) => {
+    setShowParamsVisualization(false);
+    setResultMsg("");
+    setTwoLoopsTimeConsuming("");
     setSelectedAlgorithm(value);
   }, []);
 
@@ -28,53 +36,161 @@ function App() {
     setNumberGroup(value);
   }, []);
 
-  const handleCalculate = useCallback(() => {
+  const paramsValid = useCallback((): boolean => {
     //check numbers
     if (numberGroup.trim() == "") {
       setNumbersError("Must be present");
-      return;
+      return false;
     }
     const arr: string[] = numberGroup.split(",");
     if (arr.length <= 2) {
       setNumbersError("Length of numbers must be grater than 2");
-      return;
+      return false;
     }
-    const arrNumbers: number[] = [];
     for (let i = 0; i < arr.length; i++) {
       const numberElement: number = parseInt(arr[i]);
       if (isNaN(numberElement)) {
         setNumbersError("The group of numbers is not valid.");
-        break;
-      } else {
-        arrNumbers.push(numberElement);
+        return false;
       }
     }
     setNumbersError("");
-    setArrNumbers(arrNumbers);
 
     //check target
     const toTarget: number = parseInt(target);
     if (target.trim() === "") {
       setTargetError("Must be present");
-      return;
+      return false;
     }
     if (isNaN(toTarget) || toTarget < 2) {
       setTargetError("Target value is not valid.");
-      return;
+      return false;
     }
-    setToTarget(toTarget);
     setTargetError("");
 
     //check selection
     const selection: number = parseInt(selectedAlgorithm);
     if (selection === 0) {
       setAlgorithmError("Select an algorithm to find the sum");
-      return;
+      return false;
     }
     setAlgorithmError("");
-
-    setShowParamsVisualization(true);
+    return true;
   }, [numberGroup, selectedAlgorithm, target]);
+
+  const convertStringArrIntoNumberArr = useCallback((): number[] => {
+    return numberGroup.split(",").map((n) => {
+      return parseInt(n);
+    });
+  }, [numberGroup]);
+
+  const twoLoops = useCallback(() => {
+    const pair: number[] = [];
+    const toTarget = parseInt(target);
+    const arr = convertStringArrIntoNumberArr();
+    outerloop: for (let i = 0; i < arr.length; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        const sum = arr[i] + arr[j];
+        if (sum === toTarget) {
+          pair.push(arr[i]);
+          pair.push(arr[j]);
+          setTwoSumPair(pair);
+          break outerloop;
+        }
+      }
+    }
+    if (pair.length > 1) {
+      setResultMsg(
+        <span>
+          A pair of numbers have been found:{" "}
+          <b>
+            {pair[0]},{pair[1]}
+          </b>
+        </span>,
+      );
+    } else {
+      setResultMsg("Unable to find a pair of numbers to equal the target");
+    }
+  }, [convertStringArrIntoNumberArr, target]);
+
+  const hashTable = useCallback(() => {
+    const toTarget = parseInt(target);
+    const map = new Map();
+    const arr = convertStringArrIntoNumberArr();
+    const arrResult: number[] = [];
+    arr.forEach((n: number) => {
+      map.set(toTarget - n, n);
+    });
+
+    for (let i = 0; i < arr.length; i++) {
+      const result = map.get(arr[i]);
+      if (result) {
+        arrResult.push(result);
+        arrResult.push(arr[i]);
+        setTwoSumPair(arrResult);
+        break;
+      }
+    }
+    if (arrResult.length > 1) {
+      setResultMsg(
+        <span>
+          A pair of numbers have been found:{" "}
+          <b>
+            {arrResult[0]},{arrResult[1]}
+          </b>
+        </span>,
+      );
+    } else {
+      setResultMsg("Unable to find a pair of numbers to equal the target");
+    }
+  }, [convertStringArrIntoNumberArr, target]);
+
+  const generateTwoLoop = useCallback(() => {
+    const startTime = performance.now();
+    twoLoops();
+    const endTime = performance.now();
+    setTwoLoopsTimeConsuming(
+      <span>
+        Time consumed using nested loops <b>{endTime - startTime} ms</b>
+      </span>,
+    );
+  }, [twoLoops]);
+
+  const generateHash = useCallback(() => {
+    const startTime = performance.now();
+    hashTable();
+    const endTime = performance.now();
+    setHashTimeConsuming(
+      <span>
+        Time consumed using a hash table <b>{endTime - startTime} ms</b>
+      </span>,
+    );
+  }, [hashTable]);
+
+  const handleCalculate = useCallback(() => {
+    const isValid: boolean = paramsValid();
+
+    if (isValid) {
+      setShowParamsVisualization(true);
+      switch (selectedAlgorithm) {
+        case "1":
+          generateTwoLoop();
+          break;
+        case "3":
+          generateHash();
+          break;
+        case "4":
+          generateTwoLoop();
+          generateHash();
+          break;
+        default:
+          break;
+      }
+    }
+  }, [hashTable, paramsValid, selectedAlgorithm, twoLoops]);
+
+  const arrNumbers = convertStringArrIntoNumberArr();
+  const toTarget = parseInt(target);
 
   return (
     <div>
@@ -93,10 +209,11 @@ function App() {
       <div className="parameters">
         <div>
           <p>Group of numbers</p>
-          <input
+          <textarea
             className="numberGroup"
             value={numberGroup}
             onChange={(e) => handleNumbersGroup(e.target.value)}
+            rows={4}
           />
           <p className="pError">{numbersError}</p>
         </div>
@@ -104,8 +221,8 @@ function App() {
           <p>Target</p>
           <input
             value={target}
-            type="number"
             onChange={(e) => handleTarget(e.target.value)}
+            type="number"
           />
           <p className="pError">{targetError}</p>
         </div>
@@ -121,7 +238,6 @@ function App() {
               -- Select and algorithm --
             </option>
             <option value="1">Nested loops</option>
-            <option value="2">Two pointers</option>
             <option value="3">Hash table</option>
             <option value="4">All of the above</option>
           </select>
@@ -132,7 +248,18 @@ function App() {
         </button>
       </div>
       {showParamsVisualization && (
-        <ParamsVisualization numbers={arrNumbers} target={toTarget} />
+        <>
+          <div className="results">
+            <p>{resultMsg}</p>
+            {twoLoopsTimeConsuming && <p>{twoLoopsTimeConsuming}</p>}
+            {hashTimeConsuming && <p>{hashTimeConsuming}</p>}
+          </div>
+          <ParamsVisualization
+            numbers={arrNumbers}
+            target={toTarget}
+            twoSumPair={twoSumPair}
+          />
+        </>
       )}
     </div>
   );
--- src/ParamsVisualization.tsx
+++ src/ParamsVisualization.tsx
@@ -2,19 +2,39 @@ import "./style.css";
 interface ParamsVisualization {
   numbers: number[];
   target: number;
+  twoSumPair: number[];
 }
-const ParamsVisualization = ({ numbers, target }: ParamsVisualization) => {
+const ParamsVisualization = ({
+  numbers,
+  target,
+  twoSumPair,
+}: ParamsVisualization) => {
   return (
     <div className="paramsVisualization">
       <div className="params">
-        <p>Array: [{numbers.join(",")}]</p>
         <p>Target: {target}</p>
+      </div>
+
+      <div className="blueSquareContainer">
+        <div className="blueSquare"></div>
+        <p>target found inside the array</p>
+      </div>
+
+      <div className="greenSquareContainer">
+        <div className="greenSquare"></div>
+        <p>pair of numbers found</p>
       </div>
 
       <div className="arrContainer">
         {numbers.map((n, index) => {
+          const colorClass =
+            n === target
+              ? "arrItemBlue"
+              : twoSumPair.find((x) => x === n)
+                ? "arrItemGreen"
+                : "";
           return (
-            <div className="arrItem" key={index}>
+            <div className={`arrItem ${colorClass}`} key={index}>
               <p className="index">{index}</p>
               <p>{n}</p>
             </div>
